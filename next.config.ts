a0b1b9f4f0e926f9053584be2c823fec543d5af3
Z User <z@container>
2026-05-15 05:05:19 +0000

    4b2aa9b0-9469-4815-adaa-24421f976733

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,23 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [];
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors * http: https:; default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;",
+          },
+        ],
+      },
+    ];
+  },
   allowedDevOrigins: [
     "preview-chat-3d6a2bc6-029a-4362-bdea-f2c5cd7bf5c3.space-z.ai",
     ".space-z.ai",
